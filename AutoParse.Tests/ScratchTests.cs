210027ad11301264d38023d33858b656622cd961
Abderrahman Cheddir <cheddirabderrahman@gmail.com>
2016-01-03 16:06:00 +0000

    Added TryParseOrDefault + Unit Tests

--- AutoParse.Tests/ScratchTests.cs
+++ AutoParse.Tests/ScratchTests.cs
@@ -7,15 +7,178 @@ namespace AutoParse.Tests
     [TestFixture]
     public class ScratchTests
     {
+        [Test]
+        public void should_parse_bool()
+        {
+            // Arrange
+            var boolStr = "true";
+
+            // Act
+            var boolValue = boolStr.TryParse<bool>();
+
+            // Assert
+            boolValue.ShouldEqual(true);
+        }
 
         [Test]
-        public void should_parse_value()
+        public void should_parse_char()
         {
-            Console.WriteLine("0009998".TryParse<int>());
-            Console.WriteLine("0009998".TryParse<long>());
-            Console.WriteLine("0009998".TryParse<double>());
+            // Arrange
+            var charStr = "a";
 
-            Console.WriteLine("10-25-2013".TryParseNullable<DateTime>().HasValue);
+            // Act
+            var charValue = charStr.TryParse<char>();
+
+            // Assert
+            charValue.ShouldEqual('a');
+        }
+
+        [Test]
+        public void should_parse_numeric()
+        {
+            // Arrange
+            var intStr = "0009998";
+            var longStr = "0009998";
+            var doubleStr = "0009998,0001";
+
+            // Act
+            var intValue = intStr.TryParse<int>();
+            var longValue = longStr.TryParse<long>();
+            var doubleValue = doubleStr.TryParse<double>();
+
+            // Assert
+            intValue.ShouldEqual(9998);
+            longValue.ShouldEqual(9998);
+            doubleValue.ShouldEqual(9998.0001);
+        }
+
+        [Test]
+        public void should_parse_date()
+        {
+            // Arrange
+            var dateStr = "01/01/2016 00:00:00.000";
+
+            // Act
+            var dateValue = dateStr.TryParse<DateTime>();
+
+            // Assert
+            dateValue.ShouldEqual(new DateTime(2016, 1, 1));
+        }
+
+        [Test]
+        public void should_not_parse_bool()
+        {
+            // Arrange
+            var boolStr = "trues";
+
+            // Act
+            var boolValue = boolStr.TryParse<bool>();
+
+            // Assert
+            boolValue.ShouldEqual(default(bool));
+        }
+
+        [Test]
+        public void should_not_parse_char()
+        {
+            // Arrange
+            var charStr = "ab";
+
+            // Act
+            var charValue = charStr.TryParse<char>();
+
+            // Assert
+            charValue.ShouldEqual(default(char));
+        }
+
+        [Test]
+        public void should_not_parse_numeric()
+        {
+            // Arrange
+            var intStr = "00A9998";
+            var longStr = "00A9998";
+            var doubleStr = "00A9998,0001";
+
+            // Act
+            var intValue = intStr.TryParse<int>();
+            var longValue = longStr.TryParse<long>();
+            var doubleValue = doubleStr.TryParse<double>();
+
+            // Assert
+            intValue.ShouldEqual(default(int));
+            longValue.ShouldEqual(default(long));
+            doubleValue.ShouldEqual(default(double));
+        }
+
+        [Test]
+        public void should_not_parse_date()
+        {
+            // Arrange
+            var dateStr = "03/01/2016A 00:00:00.000";
+
+            // Act
+            var dateValue = dateStr.TryParse<DateTime>();
+
+            // Assert
+            dateValue.ShouldEqual(new DateTime(0001, 1, 1));
+        }
+
+        [Test]
+        public void should_parse_bool_to_default_on_failure()
+        {
+            // Arrange
+            var boolStr = "trues";
+
+            // Act
+            var boolValue = boolStr.TryParseOrDefault<bool>(true);
+
+            // Assert
+            boolValue.ShouldEqual(true);
+        }
+
+        [Test]
+        public void should_parse_char_to_default_on_failure()
+        {
+            // Arrange
+            var charStr = "ab";
+
+            // Act
+            var charValue = charStr.TryParseOrDefault<char>('a');
+
+            // Assert
+            charValue.ShouldEqual('a');
+        }
+
+        [Test]
+        public void should_parse_numeric_to_default_on_failure()
+        {
+            // Arrange
+            var intStr = "00A9998";
+            var longStr = "00A9998";
+            var doubleStr = "00A9998,0001";
+
+            // Act
+            var intValue = intStr.TryParseOrDefault<int>(10);
+            var longValue = longStr.TryParseOrDefault<long>(10L);
+            var doubleValue = doubleStr.TryParseOrDefault<double>(10.0D);
+
+            // Assert
+            intValue.ShouldEqual(10);
+            longValue.ShouldEqual(10L);
+            doubleValue.ShouldEqual(10.0D);
+        }
+
+        [Test]
+        public void should_parse_date_to_default_on_failure()
+        {
+            // Arrange
+            var dateStr = "03/01/2016A 00:00:00.000";
+
+            // Act
+            var dateValue = dateStr.TryParseOrDefault<DateTime>(DateTime.Today);
+
+            // Assert
+            dateValue.ShouldEqual(DateTime.Today);
         }
     }
 }
--- AutoParse/AutoParse.cs
+++ AutoParse/AutoParse.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Reflection;
 using System.Linq;
 
@@ -7,21 +8,91 @@ namespace AutoParse
     public static class AutoParse
     {
         public delegate bool TryParser<T>(string str, out T value);
-        public static T TryParse<T>(this string value, TryParser<T> parser)
+        public delegate bool TryNumericParser<T>(string str, NumberStyles style, IFormatProvider provider, out T value);
+        public delegate bool TryDateParser<T>(string str, IFormatProvider provider, DateTimeStyles style, out T value);
+        public delegate bool TryDateParserExactSingleFormat<T>(string str, string format, IFormatProvider provider, DateTimeStyles style, out T value);
+        public delegate bool TryDateParserExactMultipleFormat<T>(string str, string[] formats, IFormatProvider provider, DateTimeStyles style, out T value);
+
+        private static T TryParse<T>(this string value, TryParser<T> parser)
+            where T : struct
         {
             T val;
             parser.Invoke(value, out val);
             return val;
         }
 
-        public static T? TryParseNullable<T>(this string value, TryParser<T> parser)
+        private static T? TryParseNullable<T>(this string value, TryParser<T> parser)
             where T : struct
         {
             T val;
             return parser.Invoke(value, out val) ? val : null as T?;
         }
 
+        private static T TryParseOrDefault<T>(this string value, TryParser<T> parser, T @default)
+            where T : struct
+        {
+            T val;
+            return parser.Invoke(value, out val) ? val : @default;
+        }
+
+        private static T TryParse<T>(this string value, TryNumericParser<T> parser, NumberStyles style, IFormatProvider provider)
+            where T : struct
+        {
+            ThrowExceptionIfNotNumeric<T>();
+
+            T val;
+            parser.Invoke(value, style, provider, out val);
+            return val;
+        }
+
+        private static T? TryParseNullable<T>(this string value, TryNumericParser<T> parser, NumberStyles style, IFormatProvider provider)
+            where T : struct
+        {
+            ThrowExceptionIfNotNumeric<T>();
+
+            T val;
+            return parser.Invoke(value, style, provider, out val) ? val : null as T?;
+        }
+
+        private static T TryParseOrDefault<T>(this string value, TryNumericParser<T> parser, NumberStyles style, IFormatProvider provider, T @default = default(T))
+            where T : struct
+        {
+            ThrowExceptionIfNotNumeric<T>();
+
+            T val;
+            return parser.Invoke(value, style, provider, out val) ? val : @default;
+        }
+
+        private static T TryParse<T>(this string value, TryDateParser<T> parser, IFormatProvider provider, DateTimeStyles style)
+            where T : struct
+        {
+            ThrowExceptionIfNotDate<T>();
+
+            T val;
+            parser.Invoke(value, provider, style, out val);
+            return val;
+        }
+
+        private static T? TryParseNullable<T>(this string value, TryDateParser<T> parser, IFormatProvider provider, DateTimeStyles style)
+            where T : struct
+        {
+            ThrowExceptionIfNotDate<T>();
+
+            T val;
+            return parser.Invoke(value, provider, style, out val) ? val : null as T?;
+        }
+
+        private static T TryParseOrDefault<T>(this string value, TryDateParser<T> parser, IFormatProvider provider, DateTimeStyles style, T @default = default(T))
+            where T : struct
+        {
+            ThrowExceptionIfNotDate<T>();
+
+            T val;
+            return parser.Invoke(value, provider, style, out val) ? val : @default;
+        }
+
         public static T TryParse<T>(this string value)
+            where T : struct
         {
             var parser = GetParser<T>();
             return value.TryParse(parser);
@@ -34,9 +105,104 @@ namespace AutoParse
             return value.TryParseNullable<T>(parser);
         }
 
+        public static T TryParseOrDefault<T>(this string value, T @default = default(T))
+            where T : struct
+        {
+            var parser = GetParser<T>();
+            return value.TryParseOrDefault(parser, @default);
+        }
+
+        public static T TryParse<T>(this string value, NumberStyles style, IFormatProvider provider)
+            where T : struct
+        {
+            var parser = GetNumericParser<T>();
+            return value.TryParse(parser, style, provider);
+        }
+
+        public static T? TryParseNullable<T>(this string value, NumberStyles style, IFormatProvider provider)
+            where T : struct
+        {
+            var parser = GetNumericParser<T>();
+            return value.TryParseNullable<T>(parser, style, provider);
+        }
+
+        public static T TryParseOrDefault<T>(this string value, NumberStyles style, IFormatProvider provider, T @default = default(T))
+            where T : struct
+        {
+            var parser = GetNumericParser<T>();
+            return value.TryParseOrDefault(parser, style, provider, @default);
+        }
+
+        public static T TryParse<T>(this string value, IFormatProvider provider, DateTimeStyles style)
+            where T : struct
+        {
+            var parser = GetDateParser<T>();
+            return value.TryParse(parser, provider, style);
+        }
+
+        public static T? TryParseNullable<T>(this string value, IFormatProvider provider, DateTimeStyles style)
+            where T : struct
+        {
+            var parser = GetDateParser<T>();
+            return value.TryParseNullable<T>(parser, provider, style);
+        }
+
+        public static T TryParseOrDefault<T>(this string value, IFormatProvider provider, DateTimeStyles style, T @default = default(T))
+            where T : struct
+        {
+            var parser = GetDateParser<T>();
+            return value.TryParseOrDefault(parser, provider, style, @default);
+        }
+
+        #region Utils
+
         private static TryParser<T> GetParser<T>()
         {
-            return Delegate.CreateDelegate(typeof (TryParser<T>), typeof (T), "TryParse") as TryParser<T>;
+            return
+                Delegate.CreateDelegate(typeof (TryParser<T>), typeof (T), "TryParse") as
+                    TryParser<T>;
         }
+
+        private static TryNumericParser<T> GetNumericParser<T>()
+        {
+            return
+                Delegate.CreateDelegate(typeof (TryNumericParser<T>), typeof (T), "TryParser") as
+                    TryNumericParser<T>;
+        }
+
+        private static TryDateParser<T> GetDateParser<T>()
+        {
+            return
+                Delegate.CreateDelegate(typeof(TryDateParser<T>), typeof(T), "TryParser") as
+                    TryDateParser<T>;
+        }
+
+        private static TryDateParserExactSingleFormat<T> GetDateParserExactSingleFormat<T>()
+        {
+            return
+                Delegate.CreateDelegate(typeof(TryDateParserExactSingleFormat<T>), typeof(T), "TryParser") as
+                    TryDateParserExactSingleFormat<T>;
+        }
+
+        private static TryDateParserExactMultipleFormat<T> GetDateParserExactMultipleFormat<T>()
+        {
+            return
+                Delegate.CreateDelegate(typeof(TryDateParserExactMultipleFormat<T>), typeof(T), "TryParser") as
+                    TryDateParserExactMultipleFormat<T>;
+        }
+
+        public static void ThrowExceptionIfNotNumeric<T>() where T : struct
+        {
+            if (typeof(T) == typeof(char) || typeof(T) == typeof(DateTime) || typeof(T) == typeof(bool))
+                throw new InvalidCastException("Invalid cast, use this to cast numeric types.");
+        }
+
+        public static void ThrowExceptionIfNotDate<T>() where T : struct
+        {
+            if (typeof(T) != typeof(DateTime))
+                throw new InvalidCastException("Invalid cast, use this to cast dates.");
+        }
+
+        #endregion
     }
 }
